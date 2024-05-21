a9a09f6a2690d732d45155421a520b01c456db3e
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-05-21 12:09:32 +0900

    4.1図形をたくさん描く

---
+++ 初級Java2.5/src/ManyFigures.java
@@ -0,0 +1,24 @@
+
+public class ManyFigures extends MyFrame{
+    public void run() {
+            
+        int x = 30;
+
+        setColor(0,128,0);
+        fillRect(x,80,10,100);
+        x+=20;
+        fillRect(x,80,10,100);
+        x+=20;
+        fillRect(x,80,10,100);
+        x+=20;
+        fillRect(x,80,10,100);
+        x+=20;
+        fillRect(x,80,10,100);
+        x+=20;
+        fillRect(x,80,10,100);
+        x+=20;
+        fillRect(x,80,10,100);
+        
+    }
+
+}
