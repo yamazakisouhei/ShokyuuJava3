82f86b5ae0eea80446ed09b0de8fd9f142b68647
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-05-27 11:04:24 +0900

    5.3　図形をいろいろな方向に動かす

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -3,9 +3,11 @@ public class Animation extends MyFrame{
 
     public void run() {
         
+    	int y =100;
         int x = 30;
         
-        while(x < 170) {
+        while(true) {
+        	while (x<=170) {
             
             //(1)画面を消す
             clear();
@@ -18,6 +20,19 @@ public class Animation extends MyFrame{
             
             sleep(0.1);
         }
+        while (x>=30) {
+        	 //(1)画面を消す
+            clear();
+            
+            //(2)四角形を表示する
+            setColor(0,128,0);
+            fillRect(x,80,10,100);
+            
+            x-=5;
+            
+            sleep(0.1);
+        }
+    }
     }
 }
 
