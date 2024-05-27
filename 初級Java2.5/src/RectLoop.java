c2e46701a76086f9f3dadf413d84c1209da4b3d1
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-05-27 10:03:53 +0900

    4.3 for文を利用する

--- 初級Java2.5/src/RectLoop.java
+++ 初級Java2.5/src/RectLoop.java
@@ -4,15 +4,14 @@ public class RectLoop extends MyFrame{
         
         setColor(0,128,0);
         
-        int x = 30;
-        while(x <= 170) {
-        while(x <= 210) {
-            
+        
+        int x = 30; 
+        
+        for(int i = 0; i < 15 ; i++ ) {
+            System.out.println(x);
             fillRect(x,80,10,100);
             x+=20;
-            
         }        
+        }
     }
 
-    }
-}
