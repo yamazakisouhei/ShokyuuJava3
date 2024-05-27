3be245a008e374f0501e7efdf03b21424ef3f961
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-05-27 10:50:17 +0900

    5.1図形をゆっくり表示する

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -4,14 +4,10 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 	
-		int x = 2;
-		int y = 3;
-		
-		System.out.println(x * y);
-		
-
+        System.out.println("プログラムを実行します");
 		MyFrame frame3 = new Rectlf();
-		frame3.setLocation(350, 550);
+        MyFrame frame1 = new DisplayRectsSlow();
+	
 		
 
         
