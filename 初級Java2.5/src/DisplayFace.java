9615c7fd65e7a5cb0d5f6e9f29f6ca32464150fd
yamazaki souhei <kisaragi.03285656@gmail.com>
2024-05-28 09:59:52 +0900

    6.2　簡単に描けるようにする

---
+++ 初級Java2.5/src/DisplayFace.java
@@ -0,0 +1,15 @@
+
+public class DisplayFace extends MyFrame{
+	public void run() {
+		int x = 30, y = 40;
+		fillOval(x,y,50,100);
+		fillOval(x + 100,y,50,100);
+		fillOval(x,y + 160,150,10);
+		
+		int x2 = 30, y2 = 40;
+		fillOval(x2,y2,50,100);
+		fillOval(x2 + 100,y2,50,100);
+		fillOval(x2,y2 + 160,150,10);
+	}
+
+}
--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -5,8 +5,8 @@ public class Start {
 		// TODO 自動生成されたメソッド・スタブ
 	
         System.out.println("プログラムを実行します");
-        MyFrame frame1 = new DisplayRectsSlow();
-        MyFrame frame2 = new Animation();
+        MyFrame frame1 = new DisplayFace();
+        MyFrame frame2 = new DisplayFace();
 	
 		
 
